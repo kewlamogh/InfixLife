aab4257fbc0134f77bfb910095cc6cd497e2f9ef
DaCool1 <86981743+kewlamogh@users.noreply.github.com>
2021-07-12 18:17:00 -0700

    Update main.kt

--- src/main.kt
+++ src/main.kt
@@ -1,5 +1,36 @@
 //Infix Life
-import classes
+        var output = mutableListOf<Any>()
+        if (getNamesOnly) {
+        	for (iterator in this.myFriends) {
+                output.add(iterator.name)
+            }
+            return output
+        } else {
+            return this.myFriends
+        }
+    }
+    fun intro(): String {
+        return "My name is ${this.name}. I am ${this.age} and am a ${this.gender.toString().lowercase()}."
+    }
+	infix fun isFriendsWith(otherPerson: Person) {
+    	this.myFriends.add(otherPerson)
+    	otherPerson.myFriends.add(this)
+	}
+    infix fun says(otherPerson: Action) {
+		this.interactions["Me to ${otherPerson.person.name}"] = otherPerson.msg
+    	otherPerson.person.interactions["${this.name} to me"] = otherPerson.msg
+	}
+    infix fun does(action: String) {
+    	this.thingsDoneToday.add(action)
+    }
+    
+}
+enum class Gender {MALE, FEMALE}
+infix fun String.to(person: Person): Action {
+    return Action(person, this)
+}
+data class SidePair(val first: List<Person>, val second: List<Person>)
+data class Action(val person: Person,  val msg: String)
 val opposites = mutableListOf<SidePair>()
 fun main() {
     val amogh = Person("Amogh", 9, Gender.MALE)
