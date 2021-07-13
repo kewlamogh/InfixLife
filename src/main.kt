a3d40345c7a68d3a3a76edc47d5de028ea7c0889
DaCool1 <86981743+kewlamogh@users.noreply.github.com>
2021-07-12 18:22:45 -0700

    Update main.kt

--- src/main.kt
+++ src/main.kt
@@ -1,5 +1,19 @@
+//Infix Life
+fun main() {
+    val bob = Person("Bobby", 15, Gender.MALE)
+    val jackie = Person("Jackie", 9, Gender.FEMALE)
+    val community = listOf<Person>(bob, jackie)
+    bob says ("Hello Jackie. Do you wanna play Exploding Snap?" to jackie)
+    jackie says ("Hello, Bob! Sure! I'd love to." to bob) 
+    bob isFriendsWith jackie
+    bob does "play exploding snap"
+    jackie does "play exploding snap"
+    bob does "eat dinner"
+    jackie does "eat dinner"
+    newDayFor(community)
+}
 
-class Person(val name: String, var age: Int, val gender: Gender) {
+class Person(val name: String, var age: Int = 0, val gender: Gender) {
     val myFriends: MutableList<Person> = mutableListOf()
     val interactions: MutableMap<String, String> = mutableMapOf()
     var thingsDoneToday = mutableListOf<String>()
@@ -13,17 +27,24 @@ class Person(val name: String, var age: Int, val gender: Gender) {
         var output = mutableListOf<Any>()
         if (getNamesOnly) {
         	for (iterator in this.myFriends) {
-                output.add(iterator.name)
+                if (iterator is Person) {output.add(iterator.name)}
             }
             return output
         } else {
-            return this.myFriends
+            var thisOutput: MutableList<Person> = this.myFriends
+            return thisOutput
         }
+        return mutableListOf("ERROR: function failed (idk this isn't supposed to be executed xD)")
     }
     fun intro(): String {
         return "My name is ${this.name}. I am ${this.age} and am a ${this.gender.toString().lowercase()}."
     }
 	infix fun isFriendsWith(otherPerson: Person) {
+    	this.myFriends.add(otherPerson)
+    	otherPerson.myFriends.add(this)
+	}
+    
+	infix fun Person.isFriendsWith(otherPerson: Person) {
     	this.myFriends.add(otherPerson)
     	otherPerson.myFriends.add(this)
 	}
@@ -36,59 +57,14 @@ class Person(val name: String, var age: Int, val gender: Gender) {
     }
     
 }
-
-enum class Gender {MALE, FEMALE}
-infix fun String.to(person: Person): Action {
-    return Action(person, this)
-}
-data class SidePair(val first: List<Person>, val second: List<Person>)
-data class Action(val person: Person,  val msg: String)
-
-
-fun main() {
-    val amogh = Person("Amogh", 9, Gender.MALE)
-    val dad = Person("Prashant", 253, Gender.MALE)
-    val mom = Person("Archana", na, Gender.FEMALE)
-    val abhay = Person("Abhay", 1, Gender.MALE)
-    val bob = Person("Bob", 4, Gender.MALE)
-    
-    val washington = communityOf(amogh, abhay, bob, dad, mom)
-    
-    val doofinschmertz = Person("Doofinschmertz", 30, Gender.MALE)
-    
-    val somethingOppositeWashington = communityOf(doofinschmertz)
-        
-    
-    washington isOppositeTo somethingOppositeWashington
-	
-    amogh says (amogh.intro() to bob)
-    bob says (bob.intro() to amogh)
-    amogh says ("Lets play UNO!" to bob)
-    bob says ("sure!" to amogh) 
-    amogh does "UNO"
-    bob does "UNO"
-    amogh does "eat dinner"
-    bob does "eat dinner"
-    newDayFor(washington)
-}
-val na: Int = 0
 fun newDayFor(systemOfCreatures: List<Person>) {
    for (i in systemOfCreatures) {
       i.thingsDoneToday = mutableListOf<String>()
    }
-   for (i in opposites) {
-       val thisSide = i.first
-       val otherSide = i.second
-       newDayFor(thisSide)
-       newDayFor(otherSide)
-   }
+}
+enum class Gender {MALE, FEMALE}
+infix fun String.to(person: Person): Action {
+    return Action(person, this)
 }
 
-
-fun communityOf(vararg members: Person): List<Person> {
-    return members.toList()
-}
-infix fun List<Person>.isOppositeTo(otherCommunity: List<Person>) {
-    opposites.add(SidePair(this, otherCommunity))
-    newDayFor(otherCommunity)
-}
+data class Action(val person: Person,  val msg: String)
